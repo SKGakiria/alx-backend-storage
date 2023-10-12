3fd5607edf192807ba1b9c84dab9db0d01f741d4
SKGakiria <swgakiria@gmail.com>
2023-10-12 11:59:04 +0000

    Added Task 0 - We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Script creates a table users
 -- Uses the id, email and name as attributes
 CREATE TABLE IF NOT EXISTS users (
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
 );
