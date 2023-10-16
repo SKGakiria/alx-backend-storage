eafa31a64564336d5046a2ba834afd42a863d6bd
SKGakiria <swgakiria@gmail.com>
2023-10-16 14:10:45 -0700

    Added Task 8 - List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module containing function that lists all documents in a collection"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """Function returns list of all documents in a collection"""
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
